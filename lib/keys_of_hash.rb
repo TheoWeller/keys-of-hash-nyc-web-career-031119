46415ed86d2cbbbf3474ee664535d68b9e17e98d
Theo Weller <weller.theodor@gmail.com>
2019-03-05 16:49:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,16 @@
+
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    myArray = []
-    hash.each do |key, value|
-      if key == arguments
-    myArray << value
-    myArray
+  def keys_of(*arguments)
+  new_arr = []
+    
+    arguments.each do |argument|
+      my_hash.each do |key, value|
+        if value == argument
+          new_arr.push(key)
+        end
+      end
+    end
+    new_arr
   end
-end
 end
