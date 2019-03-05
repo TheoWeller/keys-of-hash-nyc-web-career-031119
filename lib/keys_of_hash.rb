501382553d10920c389e65dcc9bee64cf920900e
Theo Weller <weller.theodor@gmail.com>
2019-03-05 16:46:29 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(hash, *arguments)
     myArray = []
     hash.each do |key, value|
-      if key == arguments
+      if key == *arguments
     myArray << value
     myArray
   end
