0f5a0799557488c4d7fc11cc180e5e7e75946682
Theo Weller <weller.theodor@gmail.com>
2019-03-05 16:46:15 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(hash, *arguments)
     myArray = []
-    myArray << arguments
+    hash.each do |key, value|
+      if key == arguments
+    myArray << value
     myArray
   end
+end
 end
